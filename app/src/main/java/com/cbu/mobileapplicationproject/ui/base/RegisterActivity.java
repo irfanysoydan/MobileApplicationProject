9b0bef2a218a88749060c4025d765903ffd3fda0
rasenshuriken <omerozoglu.1@gmail.com>
2022-05-03 15:31:23 +0300

    Architecture design

--- app/src/main/java/com/cbu/mobileapplicationproject/ui/base/RegisterActivity.java
+++ app/src/main/java/com/cbu/mobileapplicationproject/ui/base/RegisterActivity.java
@@ -3,15 +3,29 @@ package com.cbu.mobileapplicationproject.ui.base;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cbu.mobileapplicationproject.databinding.ActivityRegisterBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         ActivityRegisterBinding viewBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
+
+        TextView TVName = viewBinding.registerTxtName;
+        TextView TVEmail = viewBinding.registerTxtEmail;
+        TextView TVPass = viewBinding.registerTxtPassword;
+        TextView TVRePass = viewBinding.registerTxtPassword2;
+        viewBinding.registerBtnRegister.setOnClickListener(view -> {
+            Snackbar.make(view,TVName.getText()+" "+TVEmail.getText()+" "+TVPass.getText()+" "+TVRePass.getText(),Snackbar.LENGTH_SHORT).show();
+        });
+
+        viewBinding.registerBtnBack.setOnClickListener(view -> {
+
+        });
     }
 }
